7637f8812ca5c3ce5f3a6d6409dfac0db89c3db1
Hannes Petur Eggertsson <hannespetur@gmail.com>
2017-11-12 20:55:32 +0000

    [bug] Added missing include

--- include/stations/worker_queue.hpp
+++ include/stations/worker_queue.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic> // std::atomic
+#include <functional>
 #include <iostream> // std::cout, std::endl
 #include <thread> // std::this_thread::sleep_for
 #include <list> // std::list
@@ -11,68 +12,69 @@ namespace stations
 
 class WorkerQueue
 {
-public:
-  std::list<std::function<void()> > function_queue; // Use std::list because pushing back to lists does not invalidate previous iterators
-  std::list<std::function<void()> >::iterator item_to_run;
-  bool finished = false;
-  std::atomic<std::size_t> queue_size;
-  std::size_t completed_items = 0;
+  public:
+    std::list<std::function<void()> > function_queue; // Use std::list because pushing back to lists does not invalidate previous iterators
+    std::list<std::function<void()> >::iterator item_to_run;
+    bool finished = false;
+    std::atomic<std::size_t> queue_size;
+    std::size_t completed_items = 0;
 
-  WorkerQueue()
-  {
-    queue_size = 0;
-  }
-
-
-  void inline
-  add_work_to_queue(std::function<void()> work)
-  {
-    function_queue.push_back(work);
-
-    if (queue_size == 0 && completed_items == 0)
-      item_to_run = function_queue.begin();
-
-    ++queue_size;
-  }
-
-
-  std::size_t inline
-  get_number_of_items_in_queue() const
-  {
-    return queue_size;
-  }
-
-
-  std::size_t inline
-  get_number_of_completed_items() const
-  {
-    return completed_items;
-  }
-
-  void inline
-  operator()()
-  {
-    while (true)
+    WorkerQueue()
     {
-      if (queue_size > 0)
-      {
-        if (completed_items != 0)
-          ++item_to_run;
+      queue_size = 0;
+    }
 
-        (*item_to_run)();
-        ++completed_items;
-        --queue_size;
-      }
-      else if (finished)
+
+    void inline
+    add_work_to_queue(std::function<void()> work)
+    {
+      function_queue.push_back(work);
+
+      if (queue_size == 0 && completed_items == 0)
+        item_to_run = function_queue.begin();
+
+      ++queue_size;
+    }
+
+
+    std::size_t inline
+    get_number_of_items_in_queue() const
+    {
+      return queue_size;
+    }
+
+
+    std::size_t inline
+    get_number_of_completed_items() const
+    {
+      return completed_items;
+    }
+
+
+    void inline
+    operator()()
+    {
+      while (true)
       {
-        return;
-      }
-      else
-      {
-        std::this_thread::sleep_for(std::chrono::microseconds(100)); // 0.1 ms
+        if (queue_size > 0)
+        {
+          if (completed_items != 0)
+            ++item_to_run;
+
+          (*item_to_run)();
+          ++completed_items;
+          --queue_size;
+        }
+        else if (finished)
+        {
+          return;
+        }
+        else
+        {
+          std::this_thread::sleep_for(std::chrono::microseconds(100)); // 0.1 ms
+        }
       }
     }
-  }
 
 
 };
